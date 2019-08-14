c38fc0fabbadc1e7d5309d8cc88ba680f325dbb0
Dannypein <danieldolores117@gmail.com>
2019-08-13 20:25:58 -0500

    New changes since 2017
    cambios hechos en la pagina, como pestaña de remates, pestaña de áreas, propiedades en remate etc.

---
+++ areas.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM inmobmarimar.areas
+LIMIT 0, 1000
+
+-- Date: 2015-09-08 12:38
+*/
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (1,'Mananillo Centro',570,435,20);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (2,'San Pedrito',625,400,15);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (3,'Tapeixtles',704,352,15);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (4,'Valle de las Garzas',620,322,17);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (5,'Las Brisas',625,215,28);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (6,'Salagua',520,160,18);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (7,'Las Hadas',438,160,18);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (8,'La Punta',410,222,18);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (9,'Santiago',408,100,15);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (10,'Miramar',300,100,18);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (11,'Club Santiago',212,118,19);
+INSERT INTO `areas` (`id`,`nombre`,`position_1`,`position_2`,`position_3`) VALUES (12,'Vida del Mar',160,170,18);
